a6e89da98afd41e44c5b644dbcaf84c11e608954
hamed <amzhamed@gmail.com>
2025-12-22 13:35:29 -0800

    test pointcloud

--- MAVProxy/modules/mavproxy_vicon.py
+++ MAVProxy/modules/mavproxy_vicon.py
@@ -127,9 +127,7 @@ class ViconModule(mp_module.MPModule):
         return pos_ned, roll, pitch, yaw
 
     def get_vicon_pose_pointcloud(self):
-        print(dir(self.vicon), attr(self.vicon))
-        # print(self.vicon.supportsPointCloud())
-        # pointcloud = self.vicon.pointCloud()
+        pointcloud = self.vicon.pointCloud
         print(pointcloud)
 
     def thread_loop(self):
